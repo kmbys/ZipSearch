f848ab49eebb41b10d03926016f7727a7de23e7b
nsbt <55523468+nsbt@users.noreply.github.com>
2019-11-20 11:21:08 +0900

    initial commit

---
+++ src/main/java/com/nsbt/zip/ZipSearchApplication.java
@@ -0,0 +1,13 @@
+package com.nsbt.zip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsbt/zip/ZipSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbt.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
