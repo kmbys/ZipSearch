31cc01306815f9f708b51abd7dabe0e40dc6a43f
nsbt <55523468+nsbt@users.noreply.github.com>
2019-11-21 15:26:02 +0900

    use path variable

--- src/main/java/com/nsbt/zip/PrefectureApp.java
+++ src/main/java/com/nsbt/zip/PrefectureApp.java
@@ -2,13 +2,14 @@ package com.nsbt.zip;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PrefectureApp {
 
-	@GetMapping("北海道")
-	public ModelAndView get() {
-		return new ModelAndView("prefecture");
+	@GetMapping("{prefecture}")
+	public ModelAndView get(@PathVariable("prefecture") String prefecture) {
+		return new ModelAndView("prefecture", "name", prefecture);
 	}
 }
